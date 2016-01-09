2b352ecf9faaa6e3e39a503a92c54202dea9a2c5
HomeJackie <492118114@qq.com>
2016-01-09 15:49:10 +0800

    新增解决方案与Model

---
+++ CrayonCode.Platform/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Platform.Model;
+
+namespace CrayonCode.Platform.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            People people = new People();
+            using (PlatformEntities context = new PlatformEntities())
+            {
+                //People people = new People() { Name = "eee", Age = 89 };
+                //context.People.Add(people);
+                //context.SaveChanges();
+
+                people = (from c in context.People where c.ID == 1 select c).SingleOrDefault();
+            }
+
+            ViewBag.Name = people.Name;
+           
+            return View();
+        }
+
+    }
+}
---
+++ CrayonCode.Platform/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using Platform.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrayonCode.Platform.Models
+{
+    public class HomeModel
+    {
+       
+    }
+}
